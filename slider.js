430b9b5094c445fa30c273cfb0b6ebbf5d6d63b1
Koprowski Michał <michalkoprowski84@gmail.com>
2019-09-25 22:26:22 +0200

    added bad carusel

---
+++ slider.js
@@ -0,0 +1,40 @@
+var slides = document.querySelector(".slider-items").children;
+
+var nextSlide = document.querySelector(".right-slide");
+
+var prevSlide = document.querySelector(".left-slide");
+
+var totalSlides = slides.length;
+
+var index = 0;
+
+nextSlide.onclick = function () {
+    console.log("hej");
+    next("next");
+};
+
+
+prevSlide.onclick = function () {
+    prev('prev');
+}
+
+
+function next(direction) {
+    if (direction = "next") {
+        index++;
+        if (index == totalSlides) {
+            index = 0;
+        }
+    } else {
+        if (index == 0) {
+            index = totalSlides - 1;
+        } else {
+            index--;
+        }
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active")
+    }
+    slides[index].classList.add("active");
+};
+next(direction);
