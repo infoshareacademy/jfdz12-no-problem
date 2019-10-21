ccc74adedacb310d120db1a2df287e0d0246cda8
DamianMilecki <damian.milecki@gmail.com>
2019-10-21 14:27:02 +0200

    validation text add

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const emailIsValid = function(email) {
-    return /\S+@\S+\.\S+/.test(email)
+    return /\S+@\S+\.\S{2,}/.test(email)
   }
 
 const emailCheck = function(){
     const emailInput = document.getElementById('inputEmail3');
     emailInput.addEventListener('input',()=>{
         const emailInputText = emailInput.value;
-        document.getElementById('inputEmail3Text').innerText = emailIsValid(emailInputText) ;
+        let emailValid = emailIsValid(emailInputText);
+        if (emailValid) {
+            document.getElementById('inputEmail3Text').innerText = "poprawny email" ;
+            document.getElementById('inputEmail3Text').style.color = "green";
+        }else{
+            if (emailInputText ===''){
+                document.getElementById('inputEmail3Text').innerText="";
+            }else{
+                document.getElementById('inputEmail3Text').innerText = "wprowadź poprawny email";
+                document.getElementById('inputEmail3Text').style.color = "red";
+            }
+        }
+        
     })
 }
 
